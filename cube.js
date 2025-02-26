070b240431edce748b636329639dc6bba2912ec5
Max Denisov <galardesign@gmail.com>
2025-02-26 23:24:09 +0300

    Update cube.js

--- cube.js
+++ cube.js
@@ -1,4 +1,3 @@
-<script>
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer();
@@ -19,4 +18,3 @@
     renderer.render(scene, camera);
   }
   animate();
-</script>
